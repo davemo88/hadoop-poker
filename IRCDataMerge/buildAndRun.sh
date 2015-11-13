852c46dcb7db043f616b6a519bb11613634d8a75
ahumblet <amhumblet@gmail.com>
2015-11-12 16:07:24 -0800

    made a script to build/run/copy IRCMerge

---
+++ IRCDataMerge/buildAndRun.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+#delete local output file
+rm -rf output
+#create the new jar
+jar cvfe IRCMerge.jar IRCMerge -C bin .
+#delete previous output file
+hadoop fs -rm -r IRCMergeOutput/output
+#run map reduce
+hadoop jar IRCMerge.jar IRCMergeInput/199504Input IRCMergeOutput/output
+#copy output to local
+hadoop fs -get IRCMergeOutput/output output
